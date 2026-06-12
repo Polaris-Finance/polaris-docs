3da91ea2eae24a1fcd674c2f582b24929bb5d8fa
tokenbrice <me@tokenbrice.com>
2026-06-12 12:47:30 +0200

    chore: wrap next dev startup

---
+++ scripts/dev.mjs
@@ -0,0 +1,23 @@
+import { spawn } from 'node:child_process'
+
+const nodeOptions = [process.env.NODE_OPTIONS, '--no-experimental-webstorage']
+  .filter(Boolean)
+  .join(' ')
+
+const child = spawn('next', ['dev'], {
+  env: {
+    ...process.env,
+    NODE_OPTIONS: nodeOptions
+  },
+  shell: process.platform === 'win32',
+  stdio: 'inherit'
+})
+
+child.on('exit', (code, signal) => {
+  if (signal) {
+    process.kill(process.pid, signal)
+    return
+  }
+
+  process.exit(code ?? 0)
+})
